788f0f1f6c1e4c966af57f1bf6c332a18b0e0f4e
maxgartman <max.gartman@gmail.com>
2014-09-15 15:48:29 -0400

    remove prompt from removal of unused software

--- newstation.sh
+++ newstation.sh
@@ -8,7 +8,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y vlc git clamscan zsh skype python ruby pip openssl curl chromium-browser p7zip ubuntu-tweak
 
 #Remove software that I don't use
-sudo apt-get remove aisleriot ubuntuone gnome-sudoku gnomine gnome-mahjongg
+sudo apt-get remove -y aisleriot ubuntuone gnome-sudoku gnomine gnome-mahjongg
 
 #Final updates and upgrades, just in case
 sudo apt-get update -y && sudo apt-get upgrade -y
