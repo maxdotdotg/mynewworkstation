7fa853fed98c65e9e03e3c0eeaaf823c06d85858
Your Name <you@example.com>
2018-11-17 12:42:01 -0500

    Clean up new workstation script, since ubuntu minimal install takes care of a lot, add tmux and vim configs to repo, add todo list

--- newstation.sh
+++ newstation.sh
@@ -1,36 +1,26 @@
 #!/bin/bash
-#Script to set up preferred tools on a new Ubuntu workstation
+# Script to set up preferred tools on a new Ubuntu workstation
 
 # update and upgrade the system with apt-get
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install software that I tend to use on most machines
-sudo apt-get install -y vlc git zsh skype python ruby python-pip openssl curl chromium-browser p7zip
+sudo apt-get install -y git zsh python-pip tmux vim
 
-#Remove software that I don't use
-sudo apt-get remove -y aisleriot gnome-sudoku gnomine gnome-mahjongg
-
-#Final updates and upgrades, just in case
-sudo apt-get update -y && sudo apt-get upgrade -y
-
-#Clone Eddy's zsh config and setup
-git clone git://github.com/luchasei/zsh-config.git ~/.zsh
+# install oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git
 cd ~/.zsh
 make install
 
 #Change default shell from bash to zsh
 sudo chsh -s /usr/bin/zsh
-zsh
 
-#Change environmental variable EDITOR from vi to nano
-# because I like nano
-cd ~/
-export EDITOR=nano
-source ~/.zshrc
+# set default editor to vim
+echo "export EDITOR=vim" >> ~/.zshrc
 
+# add scripts to path
+echo "export PATH=$PATH:~/scripts" >> ~/.zshrc
 
-echo
-echo
-echo "REMEMBER TO CONFIRM ZSH IS WORKING BY OPENING A NEW TERMINAL!"
-echo "IF IT'S NOT, ADD CUSTOM COMMAND IN TERMINAL PROFILE TO USE ZSH!!"
-echo
+# add go to path
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
+
