ab1f5e47c6df9e78829d059ddbddedc7837b8a93
Max G <maxdotdotg@gmail.com>
2021-08-25 22:29:29 -0400

    add configs to script and update bins

--- newstation.sh
+++ newstation.sh
@@ -5,16 +5,25 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install software that I tend to use on most machines
-sudo apt-get install -y git zsh python-pip python-virtualenv tmux vim curl jq
+sudo apt-get install -y git zsh python3-pip tmux neovim curl jq
+
+# set up vimrc
+wget https://raw.githubusercontent.com/maxdotdotg/mynewworkstation/master/.vimrc
+
+# set up tmux config
+wget https://raw.githubusercontent.com/maxdotdotg/mynewworkstation/master/.tmux.conf
 
 # install oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# set default editor to vim
-echo "export EDITOR=vim" >> ~/.zshrc
+# set default editor to nvim
+echo "export EDITOR=nvim" >> ~/.zshrc
 
-# add scripts to path
-echo "export PATH=$PATH:~/scripts" >> ~/.zshrc
+# add pip bins to path    
+echo "export PATH=$PATH:/home/max/.local/bin" >> ~/.zshrc
 
-# add go to path
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
+# install tfenv and add to path
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+echo "export PATH=$PATH:~/.tfenv/bin" >> ~/.zshrc
+
+
